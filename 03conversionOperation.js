d650b379c2f3df109d72b9ad00dc38d1801d3a9b
harshit <harshit91735@gmail.com>
2025-06-12 18:57:47 +0530

    conversion

--- 03conversionOperation.js
+++ 03conversionOperation.js
@@ -24,3 +24,27 @@ let someNumber = 22
 let convertedValue = String(someNumber); // convert to string
 console.log(convertedValue); // "22"
 console.log(typeof (convertedValue)); // string
+
+//**************************** operations *******************************
+
+// console.log(2+2) // 4
+// console.log(2-2) // 0
+// console.log(2*2) // 4
+// console.log(2/2)// 1
+// console.log(2**3) // 8
+// console.log(2%3) // 2
+
+console.log(1+"2"); // "12" (string concatenation)
+console.log("1"+2); // "12" (string concatenation)
+console.log(1+2+"3"); // "33" (string concatenation)
+console.log("1"+2+3); // "123" (string concatenation)
+console.log(1+2+3+"4"); // "64" (string concatenation)
+
+console.log(true); // true
+console.log(false); // false
+
+let num1, num2 ,num3 
+num1=num2=num3=10; // all three variables are assigned the value 10
+ 
+
+
